350feb6769e4de8e770c09f24275d451c3d2c096
maxwill0102 <zyzw342221203@126.com>
2025-06-16 18:29:30 +0800

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,1 +1,15 @@
+import fs from 'fs';
+import path from 'path';
 
+export default function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).json({ error: 'Method Not Allowed' });
+
+  const token = req.headers.authorization?.replace('Bearer ', '');
+  const filePath = path.resolve('./data/users.json');
+  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+  const user = data.find(u => u.token === token);
+  if (!user) return res.status(403).json({ error: 'Unauthorized' });
+
+  res.status(200).json({ username: user.username, modules: user.modules });
+}
