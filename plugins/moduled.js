05492bebfaa9b786e04db017b78ffafeb23aa959
maxwill0102 <zyzw342221203@126.com>
2025-06-21 08:47:07 +0800

    Update moduled.js

--- plugins/moduled.js
+++ plugins/moduled.js
@@ -108,7 +108,7 @@
     GM_xmlhttpRequest({
       method:'POST',
       url:'https://agentseller.temu.com/api/kiana/gamblers/marketing/enroll/semi/scroll/match',
-      headers:{'Content-Type':'application/json','mallid':'634418223153529','anti-content':'<anti>','referer':location.href,'origin':location.origin,'cookie':document.cookie,'user-agent':navigator.userAgent},
+      headers:{'Content-Type':'application/json','mallid':'634418223153529','anti-content':'0aqAfoixYySYj9E2J0didyxgjRAwIqP2ID3kKGzdvqe84kyjIs4HyQfYOmjkrrze-crCiTnixgSUJIf0UKVZgmvQ75Eo_Bl6DEfLU9TF9-475E8cqUGNjYTATLJVJJqWySNB6kUA-xv1ltrWo4j80KfDIeHrC4H_5ekuK9QxQhAxvj9Q_P7hDAT4RTMrofxM5qYQUWAPzhC0WP-cTojUGQUfhZBM448owrxCtZ01vN9jxWjo087lM5hcCnRcBL02IflDP6slH4jZfiC0WUuiDbCQaXnHP7N_2x4t8H9RY2Xbs7UzRP17UlcguQbXRT1XElhr0AuaDJRDMSn88Ai5HNunGj2yyqMNtAcvWouNUwqAud9jnG__Z_Exp1l7pVnYYSB-Ub2L5IXRayS5QKvxL9vyu6BntuXBYSR2a8nqQ5RwjMStfIcXj6a5sljEe5FpqKek4ZlKK3GVq-2gw-2b_dcP0s_PPp3DKJuLtomM_QrzMFzESn2Ues4L4ZfSSRvdfXpV90GmEsbKvnlyvbJdmKkAmwpH-GzctDI4Z8bBkSO1eFK1yZCGZTSFhgq6wTtag96vwP0rvpgOMzEVgnwqkgs7hGqPOdzrdhgqKRZu4Y61vLS31aj1ZcDOoaPHL52nPmkd4bKAA8W_LvnOSy28dLdpDOIj2afFRvTt51-fsn-_ICH1KfzO0ZR-szvBDmKjJB_QffwpggAygXKvEYnFkTP5gWr28VB64SU3lrVVNArqnrc6ZrDgYcQYVAqQz1JXvLXeXGVaRTGqi8K1eWqLiVWK0ronxlyU2gJ','referer':location.href,'origin':location.origin,'cookie':document.cookie,'user-agent':navigator.userAgent},
       data:JSON.stringify({activityType:Number(type),activityThematicId:Number(thematicId),rowCount:50,addSite:true,searchScrollContext:''}),
       onload(res){ try{
         const d=JSON.parse(res.responseText);
